057e4b0ed49d846128e5c34d2d333d9fbc2cf6d5
gyumyung <gyumyung1995@gmail.com>
2021-02-23 19:34:03 +0900

    Add

--- LeetCode/IslandPerimeter.swift
+++ LeetCode/IslandPerimeter.swift
@@ -15,17 +15,14 @@ class Solution {
                     if i != 0 && grid[i-1][j] == 1 {
                         perimeter -= 1
                     }
-                    
                     // check right
                     if i != grid.count-1 && grid[i+1][j] == 1 {
                         perimeter -= 1
                     }
-                    
                     // check top
                     if j != 0 && grid[i][j-1] == 1 {
                         perimeter -= 1
                     }
-                    
                     // check bottom
                     if j != grid[i].count-1 && grid[i][j+1] == 1 {
                         perimeter -= 1
